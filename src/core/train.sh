541c1eb0f740feafaa5090f317186e0a0055b4e6
joeyfreakman <root@LAPTOP-RM5OV8M5.>
2024-07-01 00:08:33 +0200

    using multiple-gpu

--- src/core/train.sh
+++ src/core/train.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export PYTHONPATH="${PYTHONPATH}:/root/Teaching_to_pack/src/aloha/aloha_scripts: /root/interbotix_ws: /root/Teaching_to_pack" #add or modify your own python path
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 python /root/Teaching_to_pack/src/core/run.py \
     --task_name teachingtopack \
     --ckpt_dir /root/Teaching_to_pack/environment/dataset/ll_ckpt/task1 \
@@ -7,9 +8,8 @@ python /root/Teaching_to_pack/src/core/run.py \
     --chunk_size 16 \
     --batch_size 16 \
     --max_skill_len 200 \
-    --num_epochs 30000 \
+    --num_epochs 3000 \
     --lr 1e-4 \
     --seed 42 \
     --log_wandb \
-    --gpu 0,1,2,3 \
     --multi_gpu 
